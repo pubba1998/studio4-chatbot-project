11f7ff4ef1197751a26483f26f6b20182a29d6dd
pubba1998 <coolpubba@gmail.com>
2023-06-01 10:31:22 +1200

    erro fix

--- pet-food-store/chatbot.php
+++ pet-food-store/chatbot.php
@@ -1,48 +1,50 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $apiKey = ''; // Replace with your actual OpenAI API key
-  $endpoint = 'https://api.openai.com/v1/chat/completions';
-  $model = 'gpt-3.5-turbo';
+    $apiKey = ''; // Replace with your actual OpenAI API key
+    $endpoint = 'https://api.openai.com/v1/chat/completions';
+    $model = 'gpt-3.5-turbo';
 
-  $message = $_POST['message'];
-
-  // Prepare the data payload
-  $data = array(
-    'messages' => array(
-      array('role' => 'system', 'content' => 'You are a helpful assistant.'),
-      array('role' => 'user', 'content' => $message)
-    ),
-    'model' => $model
-  );
-
-  // Prepare the headers
-  $headers = array(
-    'Content-Type: application/json',
-    'Authorization: Bearer ' . $apiKey
-  );
-
-  // Make a POST request to the ChatGPT API
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $endpoint);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  $response = curl_exec($ch);
-  curl_close($ch);
-
-  // Handle the API response
-  if ($response === false) {
-    echo 'Error occurred.';
-  } else {
-    $responseData = json_decode($response, true);
+        $messages = json_decode($_POST['messages'], true);
     
-    if (isset($responseData['choices'][0]['message']['content'])) {
-      $chatbotReply = $responseData['choices'][0]['message']['content'];
-      echo $chatbotReply;
-    } else {
-      echo 'Invalid response from the API';
+        // Prepare the data payload
+        $data = array(
+            'messages' => $messages,
+            'model' => $model
+        );
+    
+        // Prepare the headers
+        $headers = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $apiKey
+        );
+    
+        // Make a POST request to the ChatGPT API
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $endpoint);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $response = curl_exec($ch);
+        curl_close($ch);
+    
+        // Handle the API response
+        if ($response === false) {
+            echo 'Error occurred.';
+        } else {
+            $responseData = json_decode($response, true);
+    
+            $chatbotReply = '';
+    
+            foreach ($responseData['choices'] as $choice) {
+                if (isset($choice['message']['content'])) {
+                    $chatbotReply .= $choice['message']['content'];
+                }
+            }
+    
+            echo $chatbotReply;
+            
+        }
     }
-  }
-}
-?>
+    ?>
+    
